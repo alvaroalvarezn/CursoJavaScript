9b5887709ca56eba819092c369e9dee273c7382d
aalvarez <alvarez_navarro@hotmail.com>
2020-06-13 21:42:01 -0500

    Iniciamos proyecto

---
+++ app.js
@@ -0,0 +1,32 @@
+const producto1 = 'Pizza',
+      precio1 = 30,
+      producto2 = 'hamburgesa',
+      precio2 = 40;
+
+let html;
+/*
+html = '<ul>'+
+       '<li>Orden: ' + producto1 +  '</li>'+
+       '<li>Precio: ' + precio1 +  '</li>'+
+       '<li>Orden: ' + producto2 +  '</li>'+
+       '<li>Precio: ' + precio2 +  '</li>'+
+       '<li>Total: ' + (precio2 + precio1)+  '</li>'+
+       '</ul>';
+
+console.log(html);*/
+
+html = `
+        <ul>
+            <li>Orden: ${producto1}</li>
+            <li>Precio: ${precio1}</li>
+            <li>Orden: ${producto2}</li>
+            <li>Precio: ${precio2}</li>
+            <li>total: ${total(precio1,precio2)}</li>
+        </ul>
+        `;
+
+function total(precio1,precio2){
+    return precio1+precio2;
+}
+
+document.getElementById('app').innerHTML=html;
